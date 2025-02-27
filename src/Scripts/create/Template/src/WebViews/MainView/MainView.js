cbda4d32b990b17b24260813559e72e457abcc4a
AhmedAyachi <aayachi032@gmail.com>
2025-02-27 18:39:25 +0100

    add android navigationbar main activity style-items

--- src/Scripts/create/Template/src/WebViews/MainView/MainView.js
+++ src/Scripts/create/Template/src/WebViews/MainView/MainView.js
@@ -4,7 +4,7 @@ import css from "./MainView.module.css";
 
 export default function MainView(props){
     const {parent,store}=props;
-    const mainview=document.webview=View({parent,id:"webview",className:css.mainview});
+    const mainview=View({parent,id:"webview",className:css.mainview});
 
     mainview.innateHTML=`
         <p>${language.$message}</p>
